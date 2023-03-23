1a3c68c1e9906d83cf9b155d35f3cde087c4b788
ZachH <zagray@calpoly.edu>
2023-03-23 16:28:57 -0700

    Completed card reading functionality that can handle upper and lower case inputs, also created deck[]

--- main/sim.py
+++ main/sim.py
@@ -8,7 +8,45 @@ class Card:
         return f'{self.value} of {self.suit}'
 
 if __name__ == '__main__':
-    valid_values = ['ace', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king']
-    valid_suits = ['hearts', 'diamonds', 'clubs', 'spades']
-    test_card = Card('jack', 'hearts')
-    print(test_card)
+    valid_values = ['Ace', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
+    valid_suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+    input_card_1 = input("Please enter your first card: \n")
+    input_card_2 = input("Please enter your second card: \n")
+
+
+    card_1_val = ''
+    card_1_suit = ''
+
+    card_2_val = ''
+    card_2_suit = ''
+
+    i = 0
+    for val in valid_values:
+        if i == 1:
+            continue
+        if val.lower() in input_card_1.lower():
+            card_1_val = val
+            i = 1
+    for suit in valid_suits:
+        if suit.lower() in input_card_1.lower():
+            card_1_suit = suit
+
+    i = 0
+    for val in valid_values:
+        if i == 1:
+            continue
+        if val.lower() in input_card_2.lower():
+            card_2_val = val
+    for suit in valid_suits:
+        if suit.lower() in input_card_2.lower():
+            card_2_suit = suit
+
+    card_1 = Card(card_1_val, card_1_suit)
+    card_2 = Card(card_2_val, card_2_suit)
+
+    deck = []
+    for val in valid_values:
+        for suit in valid_suits:
+            deck.append(Card(val, suit))
+
+    print(f'Your cards are the {card_1.__str__()} and the {card_2.__str__()}.')
