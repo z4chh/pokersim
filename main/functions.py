31e4429dc03147c8591edd92aebb212a396088ad
ZachH <zagray@calpoly.edu>
2023-03-24 14:45:20 -0700

    added deckmaker function to reduce the lines of code needed in sim

--- main/functions.py
+++ main/functions.py
@@ -1,11 +1,14 @@
 
 #This file will serve as the location for all of my function definitions
 from classes import *
+from typing import List
 
 valid_values = ['Ace', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
 valid_suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+'''these are used to pull info on the deck from'''
 
 def cardmaker(carddata: str) -> Card:
+    '''this takes the user input and turns it into the user created data type Card'''
     card_val = ''
     card_suit = ''
     i = 0
@@ -22,3 +25,13 @@ def cardmaker(carddata: str) -> Card:
             card_suit = suit
     return Card(card_val, card_suit)
 
+
+def deckmaker() -> List[Card]:
+    '''this function is used to generate a deck'''
+    list_deck = []
+    global valid_suits
+    global valid_values
+    for value in valid_values:
+        for suit in valid_suits:
+            list_deck.append(Card(value, suit))
+    return list_deck
--- main/sim.py
+++ main/sim.py
@@ -7,12 +7,10 @@ if __name__ == '__main__':
     input_card_1 = input("Please enter your first card: \n")
     input_card_2 = input("Please enter your second card: \n")
 
-    deck = []
-    for val in valid_values:
-        for suit in valid_suits:
-            deck.append(Card(val, suit))
+    deck = deckmaker()
 
     card_1 = cardmaker(input_card_1)
     card_2 = cardmaker(input_card_2)
+    user_hand = [card_1, card_2]
 
     print(f'Your cards are the {card_1} and the {card_2}.')
