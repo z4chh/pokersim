dd55015e2932ceb3e31eb5716f94d2799ed2b34e
Zach <zagray@calpoly.edu>
2023-08-18 18:09:34 -0700

    straight flush full house

--- main/functions.py
+++ main/functions.py
@@ -105,7 +105,8 @@ def precedence(hand: List[Card], flop: List[Card]) -> int:
         return 1
 
     # ---------------------------------Straight Flush checker------------------------------------------------
-    straights = [
+    card_counter = 0
+    straight_flushes = [
         [Card(10, 'Hearts'), Card(11, 'Hearts'), Card(12, 'Hearts'), Card(13, 'Hearts'), Card(1, 'Hearts')],
         [Card(10, 'Diamonds'), Card(11, 'Diamonds'), Card(12, 'Diamonds'), Card(13, 'Diamonds'), Card(1, 'Diamonds')],
         [Card(10, 'Spades'), Card(11, 'Spades'), Card(12, 'Spades'), Card(13, 'Spades'), Card(1, 'Spades')],
@@ -156,10 +157,74 @@ def precedence(hand: List[Card], flop: List[Card]) -> int:
         [Card(1, 'Spades'), Card(2, 'Spades'), Card(3, 'Spades'), Card(4, 'Spades'), Card(5, 'Spades')],
         [Card(1, 'Clubs'), Card(2, 'Clubs'), Card(3, 'Clubs'), Card(4, 'Clubs'), Card(5, 'Clubs')]
     ]
+    for straight_flush in straight_flushes:
+        for card in total_cards:
+            if card in straight_flush:
+                card_counter += 1
+        if card_counter >= 5:
+            return 2
+        card_counter = 0
+
+    # ---------------------------------Four of a kind checker-------------------------------------------------
+    card_count = {}
+
+    for card in total_cards:
+        rank = card.value
+        if rank in card_count:
+            card_count[rank] += 1
+            if card_count[rank] == 4:
+                return 3
+        else:
+            card_count[rank] = 1
+
+    # --------------------------------Full House Checker------------------------------------------------------
+    card_count = {}
+    for card in total_cards:
+        rank = card.value
+        if rank in card_count:
+            card_count[rank] += 1
+        else:
+            card_count[rank] = 1
+    for card in card_count:
+        if card_count[card] >= 3:
+            card_count[card] = -3
+            for updated_card in card_count:
+                if card_count[updated_card] >= 2:
+                    return 4
+
+    # -----------------------------Flush---------------------------------------------------------------------
+    card_count = {}
+    for card in total_cards:
+        suit = card.suit
+        if suit in card_count:
+            card_count[suit] += 1
+            if card_count[suit] == 5:
+                return 5
+        else:
+            card_count[suit] = 0
+
+    # -----------------------------Straight Checker----------------------------------------------------------
+    ranks = []
+    for card in total_cards:
+        ranks.append(card.value)
+    ranks = sorted(ranks)
+    duplicate_count = 0
+    for value in ranks:
+        duplicate_count = ranks.count(value)
+        if duplicate_count == 2:
+            ranks.remove(value)
+        if duplicate_count == 3:
+            ranks.remove(value)
+            ranks.remove(value)
+    num_of_combinations = 2
+    for i in range(num_of_combinations):
+        if ranks[i + 4] - ranks[i] == 4:
+            return 6
 
 
 
 
+    return("you lost bro")
 def reshuffle():
     '''this function 'reshuffles' the deck'''
     global deck
--- main/sim.py
+++ main/sim.py
@@ -3,8 +3,8 @@ from functions import *
 
 if __name__ == '__main__':
 
-    input_card_1 = input("Please enter your first card: \n")
-    input_card_2 = input("Please enter your second card: \n")
+    input_card_1 = input("Please enter your first card: (<value> of <suit>) \n")
+    input_card_2 = input("Please enter your second card: (<value of <suits> \n")
 
     deck = deckmaker()
     card_1 = cardmaker(input_card_1)
@@ -12,7 +12,7 @@ if __name__ == '__main__':
 
     cardchecker(card_1, card_2)
 
-    print(f'Your cards are the {card_1} and the {card_2}.')
+    print(f'Your cards are the {card_1} and the {card_2}.\n')
 
     user_hand = [card_1, card_2]
     deck.remove(card_1)
@@ -25,15 +25,32 @@ if __name__ == '__main__':
     opp_5 = genhand(deck)
 
     flop = flopgen(deck)
+    print("----------USER HAND------------")
+    print(card_1)
+    print(card_2)
+    print("----------FLOP-----------------")
+    for card in flop:
+        print(card)
+    print('\n')
+    input("PRESS ENTER TO FLIP ANOTHER CARD")
+    flop.append(flipone(deck))
 
-    user_hand = precedence(user_hand, flop)
+    print("----------USER HAND------------")
+    print(card_1)
+    print(card_2)
+    print("----------FLOP-----------------")
+    for card in flop:
+        print(card)
+    print('\n')
 
-    opp_1 = precedence(opp_1, flop)
-    opp_2 = precedence(opp_2, flop)
-    opp_3 = precedence(opp_3, flop)
-    opp_4 = precedence(opp_4, flop)
-    opp_5 = precedence(opp_5, flop)
-    print(flop, user_hand)
-    print(user_hand)
-
+    input("PRESS ENTER TO FLIP THE FINAL CARD, GOOD LUCK\n")
+    flop.append(flipone(deck))
+    print("----------USER HAND------------")
+    print(card_1)
+    print(card_2)
+    print("----------FLOP-----------------")
+    for card in flop:
+        print(card)
+    print('\n')
+    print(precedence(user_hand, flop))
 
